0f1f4081678599e3141d54fd651af53c5195a900
evanboissonnot <evan.boissonnot@netil.fr>
2017-06-22 14:20:11 +0200

    fin partie tests unitaires et debuggage

---
+++ 006 107 module tests unitaire mocha/testapp/test/testEleve.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const Eleve = require('../models/eleve');
+
+describe("Eleve", () => {
+    describe("#ajouterNote", () => {
+        it("on doit pouvoir ajouter une note vide, une erreur typée nous est retournée", () => {
+            let eleve = new Eleve("Nom", "Prenom");
+            let error = null;
+
+            try {
+                eleve.ajouterNote(null);
+            } catch(err) {
+                error = err;    
+            }
+            expect(error).to.not.null;            
+        });
+    });
+});
